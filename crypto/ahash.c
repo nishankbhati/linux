f0b4eff6976478a572ec1b6af4359ef85401e8f6
Nishank Bhati <nishank123xyz@gmail.com>
2016-06-28 09:56:48 +0530

    Update ahash.c

--- crypto/ahash.c
+++ crypto/ahash.c
@@ -56,7 +56,7 @@ static int hash_walk_next(struct crypto_hash_walk *walk)
 	if (offset & alignmask) {
 		unsigned int unaligned = alignmask + 1 - (offset & alignmask);
 
-		if (nbytes > unaligned)
+		if (nbytes >= unaligned)  //fresh index updation
 			nbytes = unaligned;
 	}
 
